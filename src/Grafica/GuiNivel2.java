d50b40ebc676d9a4d50f4b0be21dbf873b5b2cea
yohanakmoyano <yohi.moyano@gmail.com>
2021-11-04 01:31:18 -0300

    Niveles

--- src/Grafica/GuiNivel2.java
+++ src/Grafica/GuiNivel2.java
@@ -5,13 +5,21 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.SwingConstants;
+import Entidad.Item.Bomba;
+import Entidad.Item.Cuchillo;
+import Entidad.Item.Dinero;
+import Entidad.Item.Energizante;
+import Entidad.Item.Galleta;
+import Entidad.Jugador.Jugador_456;
+import Logica.Coordenada;
+
+import java.awt.Panel;
 
 
 public class GuiNivel2 extends JFrame {
@@ -20,9 +28,14 @@ public class GuiNivel2 extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JPanel panelNivel1; 
-	private JLabel laberintoNivel1;
-	private JLabel fondoNivel1;
+	private JPanel panelNivel2; 
+	private JLabel fondoNivel2;
+	private JLabel label_galleta; 
+	private JLabel label_dinero; 
+	private JLabel label_bomba; 
+	private JLabel label_cuchillo; 
+	private JLabel label_energizante; 
+	private JLabel label_jugador; 
 
 	public GuiNivel2() {
 		
@@ -46,11 +59,149 @@ public class GuiNivel2 extends JFrame {
 		getContentPane().setLayout(null);
 		
 		//panel del laberinto
-		panelNivel1= new JPanel();
+		panelNivel2= new JPanel();
 		//panelNivel1.setBounds(0, 0, 890, 561);
-		panelNivel1.setBounds(0, 0, 900, 600);
-		getContentPane().add(panelNivel1);
-		panelNivel1.setVisible(true);
+		panelNivel2.setBounds(0, 0, 900, 600);
+		getContentPane().add(panelNivel2);
+		panelNivel2.setVisible(true);
+		
+		
+		//Crea el panel
+		
+		Panel panel = new Panel();
+		panel.setBounds(197, 0, 501, 553);
+		panelNivel2.add(panel);
+		panel.setLayout(null);
+		
+		//Label Cuchillo
+		label_cuchillo=new JLabel();
+		label_cuchillo.setBounds(83, 97, 40, 58);
+		
+		Coordenada coordenadaCuchillo=new Coordenada(85,90); 
+	    Cuchillo cuchillo=new Cuchillo(1,coordenadaCuchillo);
+		
+		String rutaCuchillo = cuchillo.getRepresentacionGrafica().getRuta();
+	    ImageIcon fotoCuchillo = null;
+		try {
+			fotoCuchillo = new ImageIcon(new URL(rutaCuchillo));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		Image medidaCuchillo = fotoCuchillo.getImage().getScaledInstance(40, 9, Image.SCALE_DEFAULT);
+		panelNivel2.setLayout(null);
+		
+		label_cuchillo.setIcon(new ImageIcon(medidaCuchillo)); 
+		panel.add(label_cuchillo);
+		
+		//Label Bomba
+		label_bomba=new JLabel();
+		label_bomba.setBounds(112, 379, 40, 58);
+		
+		Coordenada coordenadaBomba=new Coordenada(85,90); 
+	    Bomba bomba=new Bomba(1,coordenadaBomba);
+		
+		String rutaBomba = bomba.getRepresentacionGrafica().getRuta();
+	    ImageIcon fotoBomba = null;
+		try {
+			fotoBomba = new ImageIcon(new URL(rutaBomba));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		Image medidaBomba = fotoBomba.getImage().getScaledInstance(30, 31, Image.SCALE_DEFAULT);
+		panelNivel2.setLayout(null);
+		
+		label_bomba.setIcon(new ImageIcon(medidaBomba)); 		
+		panel.add(label_bomba);
+		
+		//Label Jugador
+		label_jugador=new JLabel();
+		label_jugador.setBounds(235, 441, 40, 58);
+		
+		Coordenada coordenadaJugador=new Coordenada(85,90); 
+	    Jugador_456 jugador=new Jugador_456(coordenadaJugador, 100, 3); //agregar valores reales de velocidad y vidas
+		
+		String rutaJugador = jugador.getRepresentacionGrafica().getRuta();
+	    ImageIcon fotoJugador = null;
+		try {
+			fotoJugador = new ImageIcon(new URL(rutaJugador));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		Image medidaJugador = fotoJugador.getImage().getScaledInstance(30, 41, Image.SCALE_DEFAULT);
+		panelNivel2.setLayout(null);
+		
+		label_jugador.setIcon(new ImageIcon(medidaJugador)); 
+		panel.add(label_jugador);
+		
+		//Label Energizante
+		label_energizante=new JLabel();
+		label_energizante.setBounds(451, 137, 40, 58);
+		
+		Coordenada coordenadaEnergizante=new Coordenada(85,90); 
+	    Energizante energizante=new Energizante(1,coordenadaEnergizante);
+		
+		String rutaEnergizante = energizante.getRepresentacionGrafica().getRuta();
+	    ImageIcon fotoEnergizante = null;
+		try {
+			fotoEnergizante = new ImageIcon(new URL(rutaEnergizante));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		Image medidaEnergizante = fotoEnergizante.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+		panelNivel2.setLayout(null);
+		
+		label_energizante.setIcon(new ImageIcon(medidaEnergizante)); 
+		panel.add(label_energizante);
+		
+	    //label galleta
+		
+		label_galleta=new JLabel();
+		label_galleta.setVerticalAlignment(SwingConstants.BOTTOM);
+		label_galleta.setBounds(163, 277, 22, 23);
+		
+	    Coordenada coordenadaGalleta=new Coordenada(65,65); 
+	    Galleta galleta=new Galleta(1,coordenadaGalleta);
+		
+		String rutaGalleta = galleta.getRepresentacionGrafica().getRuta();
+	    ImageIcon fotoGalleta = null;
+		try {
+			fotoGalleta = new ImageIcon(new URL(rutaGalleta));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		Image medidaGalleta = fotoGalleta.getImage().getScaledInstance(15, 16, Image.SCALE_DEFAULT);
+		panelNivel2.setLayout(null);
+		
+		label_galleta.setIcon(new ImageIcon(medidaGalleta)); 
+		panel.add(label_galleta);
+		
+		//Label Dinero
+		label_dinero=new JLabel();
+		//label_cuchillo.setVerticalAlignment(SwingConstants.BOTTOM);
+		label_dinero.setBounds(235, 345, 46, 23);
+		
+		Coordenada coordenadaDinero=new Coordenada(41,90); 
+	    Dinero dinero=new Dinero(1,coordenadaDinero);
+		
+		String rutaDinero = dinero.getRepresentacionGrafica().getRuta();
+	    ImageIcon fotoDinero = null;
+		try {
+			fotoDinero = new ImageIcon(new URL(rutaDinero));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		Image medidaDinero = fotoDinero.getImage().getScaledInstance(30, 15, Image.SCALE_DEFAULT);
+		panelNivel2.setLayout(null);
+		
+		label_dinero.setIcon(new ImageIcon(medidaDinero)); 
+		panel.add(label_dinero); 
+		
 		
 		//Imagen del fondo del laberinto
 		String rutaLaberinto = this.getClass().getResource("/Images/laberinto_nivel2.png").toString();
@@ -62,21 +213,22 @@ public class GuiNivel2 extends JFrame {
 		}
 
 		Image medidaLaberinto = fotoLaberinto.getImage().getScaledInstance(500, 600, Image.SCALE_DEFAULT);
-		panelNivel1.setLayout(null);
+		panelNivel2.setLayout(null);
 		
-		//Label del laberinto
-		laberintoNivel1=new JLabel();
-		laberintoNivel1.setBounds(188, -44, 589, 640);
+		JLabel laberintoNivel1 = new JLabel();
+		laberintoNivel1.setBounds(0, 0, 491, 640);
 		laberintoNivel1.setIcon(new ImageIcon(medidaLaberinto));
-		panelNivel1.add(laberintoNivel1);
+		panel.add(laberintoNivel1);
+		
 		
 		//Label del fondo 
-		fondoNivel1=new JLabel();
-		fondoNivel1.setVerticalAlignment(SwingConstants.BOTTOM);
-		fondoNivel1.setBounds(0, 0, 890, 561);
-		panelNivel1.add(fondoNivel1);
-		fondoNivel1.setBackground(Color.BLUE);
-		fondoNivel1.setIcon(new ImageIcon(medidaJuego));
+		fondoNivel2=new JLabel();
+		fondoNivel2.setVerticalAlignment(SwingConstants.BOTTOM);
+		fondoNivel2.setBounds(0, 0, 890, 561);
+		panelNivel2.add(fondoNivel2);
+		fondoNivel2.setBackground(Color.BLUE);
+		fondoNivel2.setIcon(new ImageIcon(medidaJuego));
+		fondoNivel2.setBackground(Color.BLUE);
 		
 		
 	}
--- src/Grafica/VentanaInicio.java
+++ src/Grafica/VentanaInicio.java
@@ -54,35 +54,93 @@ public class VentanaInicio extends JFrame {
 
 		Image medidaJuego = fotoJuego.getImage().getScaledInstance(900, 600, Image.SCALE_DEFAULT);
 		
-		//Boton ingresar
-		JButton btnIngresa = new JButton("");
-		btnIngresa.setVerticalAlignment(SwingConstants.BOTTOM);
-		btnIngresa.setBounds(300, 379, 227, 77);
-		btnIngresa.setBackground(Color.black);
+		//Boton Nivel 1
+		JButton btnNivel1 = new JButton("");
+		btnNivel1.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnNivel1.setBounds(87, 394, 227, 77);
+		btnNivel1.setBackground(Color.black);
 		
 		
-		String rutaBotonJuego = this.getClass().getResource("/Images/boton_iniciar.png").toString();
-		ImageIcon fotoBoton = null;
+		String rutaBotonNivel1 = this.getClass().getResource("/Images/boton_lvl1.png").toString();
+		ImageIcon fotoBotonNivel1 = null;
 		try {
-			fotoBoton = new ImageIcon(new URL(rutaBotonJuego));
+			fotoBotonNivel1 = new ImageIcon(new URL(rutaBotonNivel1));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		btnIngresa.setIcon(fotoBoton); 
-		btnIngresa.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+		btnNivel1.setIcon(fotoBotonNivel1); 
+		btnNivel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 		
 		
-		btnIngresa.addActionListener(new ActionListener(){
+		btnNivel1.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				
-					GuiNivel1 ingresoJuego= new GuiNivel1();
-					ingresoJuego.setVisible(true);
+					GuiNivel1 ingresoNivel1= new GuiNivel1();
+					ingresoNivel1.setVisible(true);
 					setVisible(false); 
 					}
 					
 				});
 		getContentPane().setLayout(null);
-		getContentPane().add(btnIngresa);
+		
+		//Boton Nivel 2
+		JButton btnNivel2 = new JButton("");
+		btnNivel2.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnNivel2.setBounds(332, 394, 227, 77);
+		btnNivel2.setBackground(Color.black);
+		String rutaBotonNivel2 = this.getClass().getResource("/Images/boton_lvl2.png").toString();
+		ImageIcon fotoBotonNivel2 = null;
+		try {
+			fotoBotonNivel2 = new ImageIcon(new URL(rutaBotonNivel2));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		getContentPane().setLayout(null);
+		btnNivel2.setIcon(fotoBotonNivel2); 
+		btnNivel2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+		
+		
+		btnNivel2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+					GuiNivel2 ingresoNivel2= new GuiNivel2();
+					ingresoNivel2.setVisible(true);
+					setVisible(false); 
+					}
+					
+				});
+		
+		//Boton Nivel 3
+		JButton btnNivel3 = new JButton("");
+		btnNivel3.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnNivel3.setBounds(575, 394, 227, 77);
+		btnNivel3.setBackground(Color.black);
+		
+		String rutaBotonNivel3 = this.getClass().getResource("/Images/boton_lvl3.png").toString();
+		ImageIcon fotoBotonNivel3 = null;
+		try {
+			fotoBotonNivel3 = new ImageIcon(new URL(rutaBotonNivel3));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		getContentPane().setLayout(null);
+		
+		btnNivel3.setIcon(fotoBotonNivel3); 
+		btnNivel3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+		
+		
+		btnNivel3.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+					GuiNivel3 ingresoNivel3= new GuiNivel3();
+					ingresoNivel3.setVisible(true);
+					setVisible(false); 
+					}
+					
+				});
+		getContentPane().add(btnNivel3);
+		getContentPane().add(btnNivel2);
+		getContentPane().add(btnNivel1);
 				
 		//Label que contiene la imagen del fondo de la Ventana Inicio
 		inicioJuego = new JLabel("");
@@ -90,6 +148,8 @@ public class VentanaInicio extends JFrame {
 		getContentPane().add(inicioJuego);
 		inicioJuego.setIcon(new ImageIcon(medidaJuego));
 		getContentPane().add(inicioJuego, BorderLayout.CENTER);
+		
+		
 		
 	}
 }
