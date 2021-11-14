f9fed09466ac4b1621a0b58a1337b45a8e297ddf
Mamani Vanesa <mamani.vanesa111@gmail.com>
2021-11-14 18:48:17 -0300

    Modificacion entidad

--- src/entidades/Entidad.java
+++ src/entidades/Entidad.java
@@ -8,12 +8,12 @@ import logica.Puntaje;
 import patrones.visitor_entidad.VisitorEntidad;
 
 public abstract class Entidad {
-	protected int ancho;
-	protected int alto;
+	protected int ancho; //mi X
+	protected int alto; //mi Y
 	protected int esquina;
 	protected RepresentacionGrafica miRep;
-	protected Coordenada posicion; 
-	protected JLabel dibujo;
+	protected Coordenada posicion; //se tratará de la esquina superior izquierda. 
+	protected JLabel dibujo;					//Considerando a la entidad como un cuadrilátero cuyos ángulos interiores son todos rectos.
 	protected Puntaje puntajeActual;
 
 	public abstract void accept(VisitorEntidad v);
@@ -24,6 +24,38 @@ public abstract class Entidad {
 
 	public int getAlto() {
 		return alto;
+	}
+	
+	/**
+	 * Consulta cual es la posicion/coordenada correspondiente a la esquina superior izquierda de la entidad.
+	 * @return Coordenada de la esquina superior izquierda.
+	 */
+	public Coordenada getEsquinaSupIzq() {
+		return posicion;
+	}
+	
+	/**
+	 * Consulta cual es la posicion/coordenada correspondiente a la esquina superior derecha de la entidad.
+	 * @return Coordenada de la esquina superior derecha.
+	 */
+	public Coordenada getEsquinaSupDer() {
+		return new Coordenada(posicion.getX()+ancho, posicion.getY());
+	}
+	
+	/**
+	 * Consulta cual es la posicion/coordenada correspondiente a la esquina inferior izquierda de la entidad.
+	 * @return Coordenada de la esquina inferior izquierda.
+	 */
+	public Coordenada getEsquinaInfIzq() {
+		return new Coordenada(posicion.getX(), posicion.getY()+alto);
+	}
+	
+	/**
+	 * Consulta cual es la posicion/coordenada correspondiente a la esquina inferior derecha de la entidad.
+	 * @return Coordenada de la esquina inferior derecha.
+	 */
+	public Coordenada getEsquinaInfDer() {
+		return new Coordenada(posicion.getX()+ancho, posicion.getY()+alto);
 	}
 		
 	public RepresentacionGrafica getRepresentacionGrafica() {
